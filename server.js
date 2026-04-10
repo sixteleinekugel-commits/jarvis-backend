bfc6ba01697900b0205323bdae2d0dbe8f6d66cb
sixteleinekugel-commits <sixte.leinekugel@gmail.com>
2026-04-10 09:52:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post("/chat", async (req, res) => {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "mistralai/Mistral-7B-Instruct",
+          model: "meta-llama/Meta-Llama-3-8B-Instruct",
           messages: messages,
           temperature: 0.7,
           max_tokens: 300
