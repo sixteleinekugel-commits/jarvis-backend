0f07a42e158723801e10c6fdce25dea75d1c57f2
JARVIShacker <sixte.leinekugel@gmail.com>
2026-04-30 16:31:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.post("/chat", async (req, res) => {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "llama3-70b-8192",
+          model: "llama-3.3-70b-versatile",
           messages: messages,
           temperature: 0.7,
           max_tokens: 300
